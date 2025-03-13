bcfd6cf6a51429da1656f81c7fcc848f22a63b9f
rithwik <124823361+rithu453@users.noreply.github.com>
2025-03-13 12:41:38 +0530

    Update Samvaad.py

--- Desktop/Samvaad/backend/Samvaad.py
+++ Desktop/Samvaad/backend/Samvaad.py
@@ -2,21 +2,7 @@ import google.generativeai as genai
 import random
 
 API_KEYS = [
-    "AIzaSyCwdDvKqc-W9Ucmve5tU2OemneMPvymVEA",
-    "AIzaSyDka4ftzVGZnQSqzoBVf3YcXeZzJOH1Qas",
-    "AIzaSyBAeTcNrAUOW5gvF1Un8LTby7vNIwFfBF4",
-    "AIzaSyAv1xgRqX5dvGNXL-UTKZvCX4t2zTyqyg8",
-    "AIzaSyAbjMsZDt2PyIlNONxjp6DVW-TVK91doJE",
-    "AIzaSyCGOjfbAUhLNph07Otueu6G0srKgRccq98",
-    "AIzaSyD4Uzd2f3VTktZnnZ6pyALpOeHLQ2AfetE",
-    "AIzaSyDv3BUhsSjABBUCqFqBdhMOq_RRUhJW8Fk",
-    "AIzaSyA7yBrleIn_-vPOaf-p0tQGDRqJ-psLnAk",
-    "AIzaSyBrN8-Zwd3ZivSzg7cjz1grnZhxMCeuZLo",
-    "AIzaSyB6ayq-PhDRtTuTSvSJx6QnKRiXP9ZRkYc",
-    "AIzaSyCxx2UMVLxXGXAzH-krPytE7G_U1sOjWAo",
-    "AIzaSyDf9MoY9hMfXO3_JnXn5ynum9V7vWDsp5A",
-    "AIzaSyBVxVdXe9ve8GsAdxDkHNevMgEwwMJ-ip8",
-    "AIzaSyD6lbLL46Cr5_gSwZ0YGMo6zGbGhcjotPk"
+  
 ]
 
 def get_random_api_key():
